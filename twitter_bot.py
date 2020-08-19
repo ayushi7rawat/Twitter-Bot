eb5e16ae18fe83da3d7c91be8efe396088db3d4f
Ayushi Rawat <ayushi7rawat@gmail.com>
2020-08-19 23:22:23 +0530

    updating bot script

---
+++ twitter_bot.py
@@ -0,0 +1,33 @@
+import tweepy
+import time
+
+#Authenticate to Twitter
+CONSUMER_KEY = 'your CONSUMER_KEY goes here'
+CONSUMER_SECRET = 'your CONSUMER_SECRET goes here'
+ACCESS_KEY = 'your ACCESS_KEY goes here'
+ACCESS_SECRET = 'your ACCESS_SECRET goes here'
+
+#Setting up connection
+auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
+api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
+
+user= api.me()
+search = '#python3'
+num_tweet = 350
+
+#fetching tweets
+for tweet in tweepy.Cursor(api.search, search).items(num_tweet):
+	try:
+		print('Tweet Liked')
+		tweet.favorite()
+		
+		print('Retweet done')
+		tweet.retweet()
+		time.sleep(10)
+		
+	except tweepy.TweepError as e:
+		print(e.reason)
+	except StopIteration:
+		break
+
