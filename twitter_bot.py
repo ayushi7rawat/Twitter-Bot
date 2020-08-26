71bb68998544028bfaf15bcc189cddcf538689d9
ayushi7rawat <ayushi7rawat@gmail.com>
2020-08-26 20:00:00 +0530

    updating twitter bot script updating

--- twitter_bot.py
+++ twitter_bot.py
@@ -1,7 +1,7 @@
 import tweepy
 import time
 
-print('I am pymoonbot')
+print('HII, I am pymoonbot')
 #Authenticate to Twitter
 CONSUMER_KEY = 'your CONSUMER_KEY goes here'
 CONSUMER_SECRET = 'your CONSUMER_SECRET goes here'
