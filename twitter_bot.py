8be0370eed94a1bdef02cdda1e94b5e2a6358291
Prafful <prafful.lachhwani@gmail.com>
2020-10-01 11:45:10 +0530

    decreased num_tweet

--- twitter_bot.py
+++ twitter_bot.py
@@ -14,7 +14,7 @@ api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
 
 user= api.me()
 search = '#python3'
-num_tweet = 351
+num_tweet = 300
 
 #fetching tweets
 for tweet in tweepy.Cursor(api.search, search).items(num_tweet):
